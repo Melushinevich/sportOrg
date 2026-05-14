c18f1fda59505042cc7b0b9747bbe41b8e72a218
Alexandra <darbori@darboris-mac.local>
2026-05-14 17:23:04 +0300

    Доработано стартовое окно для входа, реализована связь между кнопкой регистрации и окном регистрации

--- main_app.py
+++ main_app.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QApplication, QStackedWidget
 from PyQt5.QtGui import QPalette, QColor
 from PyQt5.QtCore import Qt
 
+from start_window import StartWindow
 from registr_window import RegistrationWindow
 from data_page_sportsmen import ProfileWindow as SportsmanProfileWindow
 from data_page_trainer import ProfileWindow as TrainerProfileWindow
@@ -13,28 +14,44 @@ class MainApplication(QStackedWidget):
         super().__init__()
 
         # Создаем все окна
+        self.start_window = StartWindow()
         self.registration_window = RegistrationWindow()
         self.sportsman_window = SportsmanProfileWindow()
         self.trainer_window = TrainerProfileWindow()
 
-        # Убираем стандартные заголовки у окон, так как они будут внутри QStackedWidget
+        # Убираем стандартные заголовки у окон (для встраивания в QStackedWidget)
+        self.start_window.setWindowFlags(Qt.Widget)
         self.registration_window.setWindowFlags(Qt.Widget)
         self.sportsman_window.setWindowFlags(Qt.Widget)
         self.trainer_window.setWindowFlags(Qt.Widget)
 
         # Добавляем окна в стек
-        self.addWidget(self.registration_window)  # индекс 0
-        self.addWidget(self.sportsman_window)  # индекс 1
-        self.addWidget(self.trainer_window)  # индекс 2
+        self.addWidget(self.start_window)  # индекс 0
+        self.addWidget(self.registration_window)  # индекс 1
+        self.addWidget(self.sportsman_window)  # индекс 2
+        self.addWidget(self.trainer_window)  # индекс 3
 
         # Подключаем сигналы для переключения окон
+        self.start_window.go_to_registration.connect(self.on_go_to_registration)
+        self.start_window.go_to_login.connect(self.on_go_to_login)
         self.registration_window.register_success.connect(self.on_register_success)
 
-        # Показываем окно регистрации
+        # Показываем стартовое окно
+        self.setCurrentWidget(self.start_window)
+
+    def on_go_to_registration(self):
+        """Переход из стартового окна в окно регистрации"""
         self.setCurrentWidget(self.registration_window)
+        print("Переход на окно регистрации")
+
+    def on_go_to_login(self):
+        """Переход из стартового окна в окно входа (пока заглушка)"""
+        print("Функция входа будет реализована позже")
+        # TODO: Создать окно входа
+        # self.setCurrentWidget(self.login_window)
 
     def on_register_success(self, role, email, password):
-        """Обработка успешной регистрации"""
+        """Переход из регистрации в анкету (спортсмен/тренер)"""
         if role == "СПОРТСМЕН":
             self.setCurrentWidget(self.sportsman_window)
             print(f"Переход на анкету спортсмена (email: {email})")
@@ -42,9 +59,9 @@ class MainApplication(QStackedWidget):
             self.setCurrentWidget(self.trainer_window)
             print(f"Переход на анкету тренера (email: {email})")
 
-    def go_to_registration(self):
-        """Вернуться на регистрацию (можно добавить кнопку в профилях)"""
-        self.setCurrentWidget(self.registration_window)
+    def go_to_start(self):
+        """Вернуться на стартовое окно (можно добавить кнопку)"""
+        self.setCurrentWidget(self.start_window)
 
 
 def main():
